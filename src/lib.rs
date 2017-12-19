213b34ef244f4ec5eb96e0ab013d1a4d70c122cf
Justus Winter <justus@pep-project.org>
2017-12-19 13:09:59 +0100

    Improve the 'sequoia' crate.
      - Improve the documentation.
    
      - Re-export 'openpgp'.
    
      - Re-export the other crates without prefix.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-//! Modular Sequoia is modular.
+//! A modular OpenPGP implementation.
+//!
+//! Sequoia consists of several modules.  This crate conveniently
+//! re-exports the other crates.
 
-pub extern crate sequoia_core;
-pub extern crate sequoia_net;
-pub extern crate sequoia_store;
+pub extern crate openpgp;
+pub extern crate sequoia_core as core;
+pub extern crate sequoia_net as net;
+pub extern crate sequoia_store as store;
 
 extern crate buffered_reader;
-extern crate openpgp;
