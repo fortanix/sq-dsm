2e9e22f70cfbf8f9ffb60d193874b22f2937db7f
Neal H. Walfield <neal@pep.foundation>
2018-01-03 22:57:39 +0100

    openpgp: Add a function to return a packet given a path.
      - Introduce a method, Message::path_ref, that returns the packet at
        a given position in the tree.

--- openpgp/src/lib.rs
+++ openpgp/src/lib.rs
@@ -929,6 +929,52 @@ impl<'a> Iterator for PacketPathIter<'a> {
     }
 }
 
+impl Message {
+    /// Returns the packet at the location described by `pathspec`.
+    ///
+    /// `pathspec` is a slice of the form `[ 0, 1, 2 ]`.  Each element
+    /// is the index of packet in a container.  Thus, the previous
+    /// path specification means: return the third child of the second
+    /// child of the first top-level packet.  In other words, the
+    /// starred packet in the following tree:
+    ///
+    /// ```text
+    ///           Message
+    ///        /     |     \
+    ///       0      1      2  ...
+    ///     /   \
+    ///    /     \
+    ///  0         1  ...
+    ///        /   |   \  ...
+    ///       0    1    2
+    ///                 *
+    /// ```
+    ///
+    /// And, `[ 10 ]` means return the 11th top-level packet.
+    ///
+    /// Note: there is no packet at the root.  Thus, the path `[]`
+    /// returns None.
+    pub fn path_ref(&self, pathspec: &[usize]) -> Option<&Packet> {
+        let mut packet : Option<&Packet> = None;
+
+        let mut cont = Some(&self.top_level);
+        for i in pathspec {
+            if let Some(ref c) = cont.take() {
+                if *i < c.packets.len() {
+                    let p = &c.packets[*i];
+                    packet = Some(p);
+                    cont = p.children.as_ref();
+                    continue;
+                }
+            }
+
+            return None;
+        }
+        return packet;
+    }
+}
+
+// Tests the `paths`() iter and `path_ref`().
 #[test]
 fn packet_path_iter() {
     fn paths(iter: std::slice::Iter<Packet>) -> Vec<Vec<usize>> {
@@ -959,6 +1005,7 @@ fn packet_path_iter() {
 
         let mut paths2 : Vec<Vec<usize>> = Vec::new();
         for (path, packet) in m.descendants().paths() {
+            assert_eq!(Some(packet), m.path_ref(&path[..]));
             paths2.push(path);
         }
 
