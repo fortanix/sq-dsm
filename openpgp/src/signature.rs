5b2d351982aa1a41123d1db30b198daebb3c5ac6
Neal H. Walfield <neal@pep.foundation>
2018-01-07 22:05:13 +0100

    openpgp: When printing a Signature packet, include the issuer

--- openpgp/src/signature.rs
+++ openpgp/src/signature.rs
@@ -11,9 +11,20 @@ impl fmt::Debug for Signature {
         let unhashed_area = format!("{} bytes", self.unhashed_area.len());
         let mpis = format!("{} bytes", self.mpis.len());
 
+        // Get the issuer.  Prefer the issuer fingerprint to the
+        // issuer keyid, which may be stored in the unhashed area.
+        let issuer = if let Some(tmp) = self.issuer_fingerprint() {
+            tmp.1.to_string()
+        } else if let Some(tmp) = self.issuer() {
+            tmp.1.to_string()
+        } else {
+            "Unknown".to_string()
+        };
+
         f.debug_struct("Signature")
             .field("version", &self.version)
             .field("sigtype", &self.sigtype)
+            .field("issuer", &issuer)
             .field("pk_algo", &self.pk_algo)
             .field("hash_algo", &self.hash_algo)
             .field("hashed_area", &hashed_area)
