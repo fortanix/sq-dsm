7b2c3e5dc5da91a9fa8a045afdd7e4900d56834b
Justus Winter <justus@sequoia-pgp.org>
2018-06-29 15:17:28 +0200

    openpgp: Derive Eq for Fingerprint.
      - This is necessary to use Fingerprints as keys in hash maps.

--- openpgp/src/lib.rs
+++ openpgp/src/lib.rs
@@ -621,7 +621,7 @@ pub struct Message {
 /// 4880].
 ///
 ///   [Section 12.2 of RFC 4880]: https://tools.ietf.org/html/rfc4880#section-12.2
-#[derive(PartialEq, Clone, Hash)]
+#[derive(PartialEq, Eq, Clone, Hash)]
 pub enum Fingerprint {
     /// 20 byte SHA-1 hash.
     V4([u8;20]),
