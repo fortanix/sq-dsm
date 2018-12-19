cbd09385831fb070f95c7355dbbfd76712aa1936
Justus Winter <justus@sequoia-pgp.org>
2018-12-19 13:08:37 +0100

    openpgp: Make sign_user_attribute_binding use Signer.

--- openpgp/src/packet/signature/mod.rs
+++ openpgp/src/packet/signature/mod.rs
@@ -172,17 +172,15 @@ impl Builder {
     /// The Signature's public-key algorithm field is set to the
     /// algorithm used by `signer`, the hash-algorithm field is set to
     /// `hash_algo`.
-    pub fn sign_user_attribute_binding(mut self, signer: &Key,
-                                       signer_sec: &mpis::SecretKey,
+    pub fn sign_user_attribute_binding(mut self, signer: &mut Signer,
                                        ua: &UserAttribute, algo: HashAlgorithm)
                                        -> Result<Signature> {
-
-        self.pk_algo = signer.pk_algo();
+        self.pk_algo = signer.public().pk_algo();
         self.hash_algo = algo;
-        let digest = Signature::user_attribute_binding_hash(&self, signer, ua);
+        let digest =
+            Signature::user_attribute_binding_hash(&self, signer.public(), ua);
 
-        let mut signer = KeyPair::new(signer, signer_sec)?;
-        self.sign(&mut signer, digest)
+        self.sign(signer, digest)
     }
 
     /// Signs `hash` using `signer`.
