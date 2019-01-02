3d0ad24698b0d3ec9e43114aa3110d6b28f720df
Justus Winter <justus@sequoia-pgp.org>
2019-01-02 14:56:58 +0100

    openpgp: Improve documentation.

--- openpgp/src/crypto/asymmetric.rs
+++ openpgp/src/crypto/asymmetric.rs
@@ -7,6 +7,11 @@ use constants::HashAlgorithm;
 use Result;
 
 /// Creates a signature.
+///
+/// This is a low-level mechanism to produce an arbitrary OpenPGP
+/// signature.  Using this trait allows Sequoia to perform all
+/// operations involving signing to use a variety of secret key
+/// storage mechanisms (e.g. smart cards).
 pub trait Signer {
     /// Returns a reference to the public key.
     fn public(&self) -> &Key;
