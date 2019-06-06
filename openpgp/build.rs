55df865bb40b3349f911e351bf2b24cb43f9c054
Justus Winter <justus@sequoia-pgp.org>
2019-06-06 13:45:12 +0200

    openpgp, rfc2822: Drop force-rerun hack.

--- openpgp/build.rs
+++ openpgp/build.rs
@@ -4,13 +4,6 @@ use std::io::{self, Write};
 use std::path::PathBuf;
 extern crate lalrpop;
 
-// Rerun if any of these files change:
-#[allow(dead_code)]
-const SOURCE: [ &'static str; 2 ]
-    = [ include_str!("src/message/grammar.lalrpop"),
-        include_str!("src/tpk/grammar.lalrpop"),
-      ];
-
 fn main() {
     lalrpop::process_root().unwrap();
     include_test_data().unwrap();
--- rfc2822/build.rs
+++ rfc2822/build.rs
@@ -1,11 +1,5 @@
 extern crate lalrpop;
 
-// Rerun if any of these files change:
-#[allow(dead_code)]
-const SOURCE: [ &'static str; 1 ]
-    = [ include_str!("src/grammar.lalrpop"),
-      ];
-
 fn main() {
     lalrpop::process_root().unwrap();
 }
