10423d8a857bc2b50de315ab7b482c3b38e016df
Justus Winter <justus@sequoia-pgp.org>
2019-12-17 18:05:28 +0100

    openpgp: Improve test.

--- openpgp/src/packet/key/mod.rs
+++ openpgp/src/packet/key/mod.rs
@@ -1595,7 +1595,7 @@ mod tests {
                                   mpis::Signature::EdDSA{
                                       r: mpis::MPI::new(r), s: mpis::MPI::new(s)
                                   });
-        assert_eq!(sig.verify_message(&key, b"Hello, World\n").ok(), Some(true));
+        assert_eq!(sig.verify_message(&key, b"Hello, World\n").unwrap(), true);
     }
 
     #[test]
