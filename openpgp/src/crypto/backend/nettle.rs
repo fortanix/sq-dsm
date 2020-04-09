df5eb44e8e442c5bc4d958d914121075258393ec
Igor Matuszewski <igor@sequoia-pgp.org>
2020-04-10 00:03:01 +0200

    openpgp: Move Nettle asymmetric impls to the backend module

--- openpgp/src/crypto/backend/nettle.rs
+++ openpgp/src/crypto/backend/nettle.rs
@@ -2,6 +2,8 @@
 
 use nettle::random::{Random, Yarrow};
 
+pub mod asymmetric;
+
 /// Fills the given buffer with random data.
 pub fn random<B: AsMut<[u8]>>(mut buf: B) {
     Yarrow::default().random(buf.as_mut());
