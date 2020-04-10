dbd94bcf2ffc9a5badc023a234d332e44349bc2c
Igor Matuszewski <igor@sequoia-pgp.org>
2020-04-10 17:54:49 +0200

    openpgp: Implement RNG using Windows CNG backend

--- openpgp/src/crypto/backend/cng.rs
+++ openpgp/src/crypto/backend/cng.rs
@@ -1,3 +1,12 @@
 //! Implementation of crypto primitives using the Windows CNG (Cryptographic API: Next Generation).
 
+use win_crypto_ng::random::RandomNumberGenerator;
+
 pub mod hash;
+
+/// Fills the given buffer with random data.
+pub fn random<B: AsMut<[u8]>>(mut buf: B) {
+    RandomNumberGenerator::system_preferred()
+        .gen_random(buf.as_mut())
+        .expect("system-preferred RNG not to fail")
+}
