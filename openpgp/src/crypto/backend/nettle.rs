7561d2a4926fdaacebe4db8131b4b0279458683b
Justus Winter <justus@sequoia-pgp.org>
2020-08-04 16:50:50 +0200

    openpgp: Improve documentation for crypto::random.
      - See #474.

--- openpgp/src/crypto/backend/nettle.rs
+++ openpgp/src/crypto/backend/nettle.rs
@@ -9,6 +9,14 @@ pub mod hash;
 pub mod symmetric;
 
 /// Fills the given buffer with random data.
+///
+/// Fills the given buffer with random data produced by a
+/// cryptographically secure pseudorandom number generator (CSPRNG).
+/// The output may be used as session keys or to derive long-term
+/// cryptographic keys from.  However, to create session keys,
+/// consider using [`SessionKey::new`].
+///
+///   [`SessionKey::new`]: struct.SessionKey.html#method.new
 pub fn random<B: AsMut<[u8]>>(mut buf: B) {
     Yarrow::default().random(buf.as_mut());
 }
