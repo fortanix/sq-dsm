mkulke/sequoia

bench: Add encrypt+sign and decrypt+verify.

openpgp/benches/common/encrypt.rs
@@ -1,6 +1,8 @@
 use sequoia_openpgp::cert::Cert;
 use sequoia_openpgp::policy::StandardPolicy;
-use sequoia_openpgp::serialize::stream::{Encryptor, LiteralWriter, Message};
+use sequoia_openpgp::serialize::stream::{
+    padding::Padder, Armorer, Encryptor, LiteralWriter, Message, Signer,
+};
 
 use std::io::Write;
 
@@ -20,6 +22,46 @@ pub fn encrypt_to_cert(
     let message =
         Encryptor::for_recipients(Message::new(&mut sink), recipients)
             .build()?;
+    let mut w = LiteralWriter::new(message).build()?;
+    w.write_all(bytes)?;
+    w.finalize()?;
+    Ok(sink)
+}
+
+// naively encrypt and sign, without caring for revocation or expiration
+pub fn encrypt_to_cert_and_sign(
+    bytes: &[u8],
+    sender: &Cert,
+    recipient: &Cert,
+) -> sequoia_openpgp::Result<Vec<u8>> {
+    let mut sink = vec![];
+
+    let p = &StandardPolicy::new();
+    let signing_keypair = sender
+        .keys()
+        .with_policy(p, None)
+        .secret()
+        .for_signing()
+        .nth(0)
+        .unwrap()
+        .key()
+        .clone()
+        .into_keypair()?;
+
+    let recipients = recipient
+        .keys()
+        .with_policy(p, None)
+        .supported()
+        .for_transport_encryption()
+        .for_storage_encryption();
+
+    let message = Message::new(&mut sink);
+    let message = Armorer::new(message).build()?;
+    let message = Encryptor::for_recipients(message, recipients).build()?;
+    let message = Padder::new(message).build()?;
+    let message = Signer::new(message, signing_keypair)
+        //.add_intended_recipient(&recipient)
+        .build()?;
     let mut w = LiteralWriter::new(message).build()?;
     w.write_all(bytes)?;
     w.finalize()?;

openpgp/benches/encrypt_sign_message.rs
@@ -1,0 +1,46 @@
+use criterion::{
+    criterion_group, criterion_main, BenchmarkId, Criterion, Throughput,
+};
+
+use sequoia_openpgp::cert::Cert;
+use sequoia_openpgp::parse::Parse;
+
+mod common;
+use common::encrypt;
+
+lazy_static::lazy_static! {
+    static ref ZEROS_1_MB: Vec<u8> = vec![0; 1 * 1024 * 1024];
+    static ref ZEROS_10_MB: Vec<u8> = vec![0; 10 * 1024 * 1024];
+}
+
+pub fn encrypt_to_donald_sign_by_ivanka(bytes: &[u8]) {
+    let sender = Cert::from_bytes(
+        &include_bytes!("../tests/data/keys/ivanka-private.gpg")[..],
+    )
+    .unwrap();
+    let recipient = Cert::from_bytes(
+        &include_bytes!("../tests/data/keys/the-donald-private.gpg")[..],
+    )
+    .unwrap();
+    encrypt::encrypt_to_cert_and_sign(bytes, &sender, &recipient).unwrap();
+}
+
+fn bench_encrypt_sign(c: &mut Criterion) {
+    let mut group = c.benchmark_group("encrypt and sign message");
+
+    // Encrypt a very short, medium and very long message.
+    let messages = &[b"Hello world.", &ZEROS_1_MB[..], &ZEROS_10_MB[..]];
+
+    for message in messages {
+        group.throughput(Throughput::Bytes(message.len() as u64));
+        group.bench_with_input(
+            BenchmarkId::new("encrypt and sign", message.len()),
+            &message,
+            |b, m| b.iter(|| encrypt_to_donald_sign_by_ivanka(&m)),
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_encrypt_sign);
+criterion_main!(benches);

fortanix/sq-dsm

openpgp/benches/common/encrypt.rs
@@ -1,7 +1,9 @@
 use sequoia_openpgp as openpgp;
 use openpgp::cert::Cert;
 use openpgp::policy::StandardPolicy;
-use openpgp::serialize::stream::{Encryptor, LiteralWriter, Message};
+use openpgp::serialize::stream::{
+    padding::Padder, Armorer, Encryptor, LiteralWriter, Message, Signer,
+};
 
 use std::io::Write;
 
@@ -21,6 +23,46 @@ pub fn encrypt_to_cert(
     let message =
         Encryptor::for_recipients(Message::new(&mut sink), recipients)
             .build()?;
+    let mut w = LiteralWriter::new(message).build()?;
+    w.write_all(bytes)?;
+    w.finalize()?;
+    Ok(sink)
+}
+
+// naively encrypt and sign, without caring for revocation or expiration
+pub fn encrypt_to_cert_and_sign(
+    bytes: &[u8],
+    sender: &Cert,
+    recipient: &Cert,
+) -> openpgp::Result<Vec<u8>> {
+    let mut sink = vec![];
+
+    let p = &StandardPolicy::new();
+    let signing_keypair = sender
+        .keys()
+        .with_policy(p, None)
+        .secret()
+        .for_signing()
+        .nth(0)
+        .unwrap()
+        .key()
+        .clone()
+        .into_keypair()?;
+
+    let recipients = recipient
+        .keys()
+        .with_policy(p, None)
+        .supported()
+        .for_transport_encryption()
+        .for_storage_encryption();
+
+    let message = Message::new(&mut sink);
+    let message = Armorer::new(message).build()?;
+    let message = Encryptor::for_recipients(message, recipients).build()?;
+    let message = Padder::new(message).build()?;
+    let message = Signer::new(message, signing_keypair)
+        //.add_intended_recipient(&recipient)
+        .build()?;
     let mut w = LiteralWriter::new(message).build()?;
     w.write_all(bytes)?;
     w.finalize()?;

openpgp/benches/encrypt_sign_message.rs
@@ -1,0 +1,47 @@
+use criterion::{
+    criterion_group, criterion_main, BenchmarkId, Criterion, Throughput,
+};
+
+use sequoia_openpgp as openpgp;
+use openpgp::cert::Cert;
+use openpgp::parse::Parse;
+
+mod common;
+use common::encrypt;
+
+lazy_static::lazy_static! {
+    static ref ZEROS_1_MB: Vec<u8> = vec![0; 1 * 1024 * 1024];
+    static ref ZEROS_10_MB: Vec<u8> = vec![0; 10 * 1024 * 1024];
+}
+
+pub fn encrypt_to_donald_sign_by_ivanka(bytes: &[u8]) {
+    let sender = Cert::from_bytes(
+        &include_bytes!("../tests/data/keys/ivanka-private.gpg")[..],
+    )
+    .unwrap();
+    let recipient = Cert::from_bytes(
+        &include_bytes!("../tests/data/keys/the-donald-private.gpg")[..],
+    )
+    .unwrap();
+    encrypt::encrypt_to_cert_and_sign(bytes, &sender, &recipient).unwrap();
+}
+
+fn bench_encrypt_sign(c: &mut Criterion) {
+    let mut group = c.benchmark_group("encrypt and sign message");
+
+    // Encrypt a very short, medium and very long message.
+    let messages = &[b"Hello world.", &ZEROS_1_MB[..], &ZEROS_10_MB[..]];
+
+    for message in messages {
+        group.throughput(Throughput::Bytes(message.len() as u64));
+        group.bench_with_input(
+            BenchmarkId::new("encrypt and sign", message.len()),
+            &message,
+            |b, m| b.iter(|| encrypt_to_donald_sign_by_ivanka(&m)),
+        );
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_encrypt_sign);
+criterion_main!(benches);