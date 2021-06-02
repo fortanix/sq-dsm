1e9de2a07ed0c1169276ff263263a10467e481b5
Francisco Vial-Prado <francisco.vialprado@fortanix.com>
2021-06-02 17:01:16 +0200

    Test script: Add no-sign encryption, verbosity

--- sq/tests/sdkms.sh
+++ sq/tests/sdkms.sh
@@ -2,6 +2,23 @@
 
 sq="cargo run -- "
 
+# Parse input flags
+case "$1" in
+    --p256) cipher_suite="nistp256";;
+    --p384) cipher_suite="nistp384";;
+    --p521) cipher_suite="nistp521";;
+    --cv25519) cipher_suite="cv25519";;
+    --rsa3k) cipher_suite="rsa3k";;
+    -*) echo "unknown option: $1" >&2; exit 1;;
+esac
+
+if (( $# != 3 )); then
+    echo "Usage: test.sh --[rsa3k, p256, p384, p521, cv25519] -v <int verbosity [0, 1, 2]>"
+    exit 1
+fi
+
+verbosity=$3
+
 # tmp directory, erased on exit
 create_tmp_dir() {
     eval "$1"="$(mktemp -d)"
@@ -15,17 +32,15 @@ comm() {
     printf "$ %s\n" "$1"
 }
 
-# Parse input flags
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    --p256) cipher_suite="nistp256"; shift 1;;
-    --p384) cipher_suite="nistp384"; shift 1;;
-    --p521) cipher_suite="nistp521"; shift 1;;
-    --cv25519) cipher_suite="cv25519"; shift 1;;
-    --rsa3k) cipher_suite="rsa3k"; shift 1;;
-    -*) echo "unknown option: $1" >&2; exit 1;;
-  esac
-done
+my_cat() {
+    if (( $verbosity == 1 )); then
+        head -n4 $1
+        echo "[TRUNCATED]"
+    fi
+    if (( $verbosity == 2 )); then
+        cat $1
+    fi
+}
 
 data=""
 create_tmp_dir data
@@ -38,12 +53,15 @@ alice_public=$data/alice.asc
 bob_sdkms=$data/bob_sdkms.asc
 bob_local_priv=$data/bob_local_priv.asc
 bob_local_pub=$data/bob_local_pub.asc
-encrypted=$data/message.txt.gpg
-decrypted=$data/decrypted.txt
+encrypted_nosign=$data/message.txt.encrypted.nosign
+encrypted_signed=$data/message.txt.encrypted.signed
+decrypted_nosign=$data/decrypted.txt.nosign
+decrypted_signed=$data/decrypted.txt.signed
 signed=$data/message.signed.asc
 
-alice_key_name="test-alice-$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "${1:-10}" | head -n 1)"
-bob_key_name="test-bob-bb$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "${1:-10}" | head -n 1)"
+random=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "10" | head -n 1)
+alice_key_name="test-alice-$random"
+bob_key_name="test-bob-$random"
 
 comm "version"
 $sq --version
@@ -53,23 +71,39 @@ $sq key generate --sdkms-key="$alice_key_name" --userid="Alice Павловна 
 $sq key generate --sdkms-key="$bob_key_name" --userid="Bob Сергeeвич Прокoфьев <bob@openpgp.example>"
 $sq key generate --userid="Bob Сергeeвич Прокoфьев <bob@openpgp.example>" --export="$bob_local_priv"
 
-comm "certificates"
+comm "certificate Alice"
 $sq key extract-cert --sdkms-key="$alice_key_name" > "$alice_public"
+my_cat "$alice_public"
+comm "certificate Bob SDKMS"
 $sq key extract-cert --sdkms-key="$bob_key_name" > "$bob_sdkms"
+my_cat "$bob_sdkms"
+comm "certificate Bob Local"
 $sq key extract-cert "$bob_local_priv" > "$bob_local_pub"
+my_cat "$bob_local_pub"
 
 printf "Y el verso cae al alma como al pasto el rocío.\n" > "$message"
 
 comm "sign"
 $sq sign --sdkms-key="$alice_key_name" "$message" > "$signed"
+my_cat "$signed"
 
 comm "verify"
 $sq verify --signer-cert="$alice_public" "$signed"
 
-comm "encrypt to Alice, sign with both Bob keys"
-$sq encrypt --signer-sdkms-key="$bob_key_name" --signer-key="$bob_local_priv" --recipient-cert "$alice_public" "$message" --output "$encrypted"
+comm "encrypt to Alice, no signatures"
+$sq encrypt --recipient-cert "$alice_public" "$message" --output "$encrypted_nosign"
+my_cat "$encrypted_nosign"
 
 comm "decrypt"
-$sq decrypt --signer-cert="$bob_sdkms" --signer-cert="$bob_local_pub" --sdkms-key="$alice_key_name" "$encrypted" --output "$decrypted"
+$sq decrypt --sdkms-key="$alice_key_name" "$encrypted_nosign" --output "$decrypted_nosign"
 
-diff "$message" "$decrypted"
+diff "$message" "$decrypted_nosign"
+
+comm "encrypt to Alice, sign with both Bob keys"
+$sq encrypt --signer-sdkms-key="$bob_key_name" --signer-key="$bob_local_priv" --recipient-cert "$alice_public" "$message" --output "$encrypted_signed"
+my_cat "$encrypted_signed"
+
+comm "decrypt"
+$sq decrypt --signer-cert="$bob_sdkms" --signer-cert="$bob_local_pub" --sdkms-key="$alice_key_name" "$encrypted_signed" --output "$decrypted_signed"
+
+diff "$message" "$decrypted_signed"
