b20d343628e341c832b40f24929515a290b7d0ce
Nora Widdecke <nora@sequoia-pgp.org>
2021-11-25 13:36:43 +0100

    opnepgp: Fix typo.

--- openpgp/src/cert/parser/low_level/lexer.rs
+++ openpgp/src/cert/parser/low_level/lexer.rs
@@ -52,7 +52,7 @@ assert_send_and_sync!(Token);
 ///
 /// Due to the way the parser code is generated, it must be marked as
 /// public.  But, since this module is not public, it will not
-/// actually be exported to used of the library.
+/// actually be exported to users of the library.
 pub enum Component {
     SubkeyBundle(SubkeyBundle<key::PublicParts>),
     UserIDBundle(UserIDBundle),
